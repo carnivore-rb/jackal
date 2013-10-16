6ddca6a428166a45c307a15ae096a7138b51b03e
Chris Roberts <chrisroberts.code@gmail.com>
2013-10-16 10:18:05 -0700

    Add customized payload builder

--- lib/carnivore-runner/callback.rb
+++ lib/carnivore-runner/callback.rb
@@ -3,6 +3,13 @@ require 'carnivore/callback'
 module CarnivoreRunner
   class Callback < Carnivore::Callback
 
+    def new_payload(payload)
+      {
+        :id => Celluloid.uuid,
+        :data => payload
+      }
+    end
+
     def forward(destination, payload)
       Celluloid::Actor[destination].transmit(payload)
     end
