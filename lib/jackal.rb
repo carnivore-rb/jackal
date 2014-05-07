2b2a69c5bf2aedde124a1c019d802f04510d3477
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-07 11:41:09 -0700

    Include utils loader

--- lib/jackal.rb
+++ lib/jackal.rb
@@ -4,6 +4,7 @@ module Jackal
   autoload :Cli, 'jackal/cli'
   autoload :Callback, 'jackal/callback'
   autoload :Error, 'jackal/error'
+  autoload :Utils, 'jackal/utils'
 end
 
 require 'jackal/version'
