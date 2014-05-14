02ff6bf4bc3ba921d5657ef63f13296e47ff3408
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-13 17:29:00 -0700

    Add jackal based api util for point building

---
+++ lib/jackal/utils/http_api.rb
@@ -0,0 +1,21 @@
+begin
+  require 'carnivore-http'
+  require 'carnivore-http/point_builder'
+rescue LoadError
+  $stderr.puts 'The `carnivore-http` gem must be installed and available!'
+  raise
+end
+
+module Jackal
+  module Utils
+
+    # Customized point builder to provide jackal helpers
+    class HttpApi < Carnivore::Http::PointBuilder
+      include Jackal::Utils::Payload
+      include Jackal::Utils::Config
+      # @!parse include Jackal::Utils::Payload
+      # @!parse include Jackal::Utils::Config
+    end
+  end
+
+end
