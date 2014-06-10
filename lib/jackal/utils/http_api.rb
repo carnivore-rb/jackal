e2f08cd523349d13b9b0987482288bc0d444e20c
Chris Roberts <chrisroberts.code@gmail.com>
2014-06-10 07:08:32 -0700

    Load helper modules directly into endpoint

--- lib/jackal/utils/http_api.rb
+++ lib/jackal/utils/http_api.rb
@@ -1,3 +1,5 @@
+require 'jackal'
+
 begin
   require 'carnivore-http'
   require 'carnivore-http/point_builder'
@@ -10,12 +12,15 @@ module Jackal
   module Utils
 
     # Customized point builder to provide jackal helpers
-    class HttpApi < Carnivore::Http::PointBuilder
-      include Jackal::Utils::Payload
-      include Jackal::Utils::Config
-      # @!parse include Jackal::Utils::Payload
-      # @!parse include Jackal::Utils::Config
-    end
+    class HttpApi < Carnivore::Http::PointBuilder; end
+
   end
 
+end
+
+class Carnivore::Http::PointBuilder::Endpoint
+  include Jackal::Utils::Payload
+  include Jackal::Utils::Config
+  # @!parse include Jackal::Utils::Payload
+  # @!parse include Jackal::Utils::Config
 end
