a4aaa2d82b78568c16c4287f942283f45c972325
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-03 07:16:03 -0800

    Set hook configuration path within constant and allow multiple paths to be specified (prevents update being breaking change)

--- lib/jackal/utils.rb
+++ lib/jackal/utils.rb
@@ -4,6 +4,12 @@ module Jackal
   # Helper utilities
   module Utils
 
+    # Valid configuration paths for hook configuration
+    HTTP_HOOK_CONFIG = [
+      [:http_hook],
+      [:jackal, :http_hook]
+    ]
+
     autoload :Spec, 'jackal/utils/spec'
     autoload :Payload, 'jackal/utils/payload'
     autoload :Config, 'jackal/utils/config'
@@ -20,14 +26,17 @@ module Jackal
       #
       # @return [TrueClass, FalseClass]
       def load_http_hook
-        if(Carnivore::Config.get(:jackal, :http_hook))
+        hook_config = HTTP_HOOK_CONFIG.map do |path|
+          Carnivore::Config.get(*path)
+        end.compact.first
+        if(hook_config)
           Carnivore.configure do
             Carnivore::Source.build(
               :type => :http_endpoints,
               :args => {
                 :name => :jackal_http_hook,
-                :bind => Carnivore::Config.fetch(:jackal, :http_hook, :bind, "0.0.0.0"),
-                :port => Carnivore::Config.fetch(:jackal, :http_hook, :port, 8989)
+                :bind => hook_config.fetch(:bind, "0.0.0.0"),
+                :port => hook_config.fetch(:port, 8989)
               }
             )
           end
