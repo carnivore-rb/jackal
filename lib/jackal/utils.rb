c4bb7ee49ae3610203f180dfd4ddf88e107fb44b
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-02 19:33:26 -0800

    Move hook configurations within jackal namespace

--- lib/jackal/utils.rb
+++ lib/jackal/utils.rb
@@ -16,18 +16,18 @@ module Jackal
 
     class << self
 
-      # Load thee HTTP Hook if configured
+      # Load the HTTP Hook if configured
       #
       # @return [TrueClass, FalseClass]
       def load_http_hook
-        if(Carnivore::Config.get(:http_hook))
+        if(Carnivore::Config.get(:jackal, :http_hook))
           Carnivore.configure do
             Carnivore::Source.build(
               :type => :http_endpoints,
               :args => {
-                :name => :http_hook,
-                :bind => Carnivore::Config.get(:http_hook, :bind) || "0.0.0.0",
-                :port => Carnivore::Config.get(:http_hook, :port) || 8989
+                :name => :jackal_http_hook,
+                :bind => Carnivore::Config.fetch(:jackal, :http_hook, :bind, "0.0.0.0"),
+                :port => Carnivore::Config.fetch(:jackal, :http_hook, :port, 8989)
               }
             )
           end
