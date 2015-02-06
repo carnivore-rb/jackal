c30c77f04261f0a246b69253522a0ef37b891b82
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-06 05:58:59 -0800

    Allow options hash to dictate process manager setup. Provide access to spawn without requiring spawn usage globally.

--- lib/jackal/utils/process.rb
+++ lib/jackal/utils/process.rb
@@ -8,15 +8,24 @@ module Jackal
   module Utils
     class Process
 
+      # Default path for IO tmp files
+      DEFAULT_STORAGE_DIRECTORY = '/tmp/jackal-process-manager'
+
+      # @return [Smash] manager configuration
+      attr_reader :configuration
+      # @return [String] storage directory path
+      attr_reader :storage_directory
+
       # Create new instance
       #
+      # @param config [Smash] process manager configuration
       # @return [self]
-      def initialize
-        @storage_directory = Carnivore::Config.fetch(
-          :fission, :utils, :process_manager, :storage,
-          '/tmp/fission/process_manager'
+      def initialize(config={})
+        @configuration = config.to_smash
+        @storage_directory = configuration.fetch(
+          :storage_directory, DEFAULT_STORAGE_DIRECTORY
         )
-        FileUtils.mkdir_p(@storage_directory)
+        FileUtils.mkdir_p(storage_directory)
       end
 
       # Create new process
@@ -30,7 +39,13 @@ module Jackal
           command = Shellwords.shellsplit(command.first)
         end
         if(block_given?)
-          yield ChildProcess.build(*command)
+          if(configuration[:spawn])
+            yield ChildProcess::Unix::PosixSpawnProcess.new(command)
+          else
+            yield ChildProcess.build(*command)
+          end
+        else
+          raise ArgumentError.new('Expecting block but no block provided!')
         end
         true
       end
@@ -40,7 +55,7 @@ module Jackal
       # @param args [String] argument list joined for filename
       # @return [IO]
       def create_io_tmp(*args)
-        path = File.join(@storage_directory, args.join('-'))
+        path = File.join(storage_directory, args.join('-'))
         FileUtils.mkdir_p(File.dirname(path))
         t_file = File.open(path, 'w+')
         t_file.sync
