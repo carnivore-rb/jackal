02a190094055ccf141f543f41703b3474bdfddec
Chris Roberts <code@chrisroberts.org>
2015-05-03 06:59:50 -0700

    Provide service registration point

--- lib/jackal.rb
+++ lib/jackal.rb
@@ -7,6 +7,25 @@ module Jackal
   autoload :Formatter, 'jackal/formatter'
   autoload :Utils, 'jackal/utils'
   autoload :Loader, 'jackal/loader'
+
+  # Add service information
+  #
+  # @param name [String, Symbol] name of service
+  # @param args [Hash] service information
+  # @option args [String] :description
+  # @option args [Hash] :configuration
+  # @return [NilClass]
+  def self.service(name, args={})
+    @services[name] = args
+    nil
+  end
+
+  # @return [Smash] registered service info
+  def self.service_info
+    @services.to_smash
+  end
+
+  @services = Smash.new
 end
 
 require 'jackal/version'
